2eb8d5f6ffb4b30f9279c9c46fde49926edc299c
yunjaehun <wogns8030@naver.com>
2024-05-19 17:18:37 +0900

    feat : Js Test

---
+++ app.js
@@ -0,0 +1,20 @@
+// var name = "max";
+//
+// if(name == "max"){
+//   let hobbies = ["sports","cooking"];
+//   console.log(hobbies);
+// }
+//
+// function greet(){
+//   var age = 30;
+//   var name = "Manuel";
+//   console.log(name,age,history);
+// }
+//
+// console.log(name,hobbies);
+//
+// greet();
+
+"use strict"; //엄격모드 => 예약어, 선언되지않은변수 등 특정 행동을 차단
+let userName = "jh1"
+console.log(userName);
